6b1232117f80a6a4e40e8ba2e62566435bcbe8c9
feng bo <bo.feng@gtdollar.com>
2019-09-26 18:01:24 +0800

    tradepage update

--- src/pages/trade/TradePageView.js
+++ src/pages/trade/TradePageView.js
@@ -7,18 +7,18 @@ import Keys from "../../configs/Keys";
 
 class TradePageView extends React.Component {
 
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
 
         this.state = {
             isRequesting: false,
         }
     }
 
-    static navigationOptions = ( props ) => {
-        const { navigation } = props;
-        const { state, setParams } = navigation;
-        const { params } = state;
+    static navigationOptions = (props) => {
+        const {navigation} = props;
+        const {state, setParams} = navigation;
+        const {params} = state;
 
         return {
             title: "TradePageView",
@@ -30,118 +30,176 @@ class TradePageView extends React.Component {
     }
 
     componentWillUnmount() {
-        this.setState = ( state, callback ) => {
+        this.setState = (state, callback) => {
 
         };
     }
 
-    componentWillReceiveProps( nextProps ) {
+    componentWillReceiveProps(nextProps) {
     }
 
-    shouldComponentUpdate( nextProps, nextState ) {
+    shouldComponentUpdate(nextProps, nextState) {
         return true;
     }
 
     render() {
         return (
-            <View style={[ commonStyles.wrapper, ]}>
-                <SafeAreaView style={[ commonStyles.wrapper, ]}>
+            <View style={[commonStyles.wrapper,]}>
+                <SafeAreaView style={[commonStyles.wrapper,]}>
                     {this.renderTopBar()}
                     {this.renderPriceBar()}
+                    {this.renderEntrustView({'buyList': ['232232'], 'sellList': ['232232']})}
+
+
+                    {this.renderPriceBar()}
+
                 </SafeAreaView>
             </View>
         );
     }
 
 
-    renderTopBar (){
-        return(
-            <View style={[{flexDirection:'row'}]}>
+    renderTopBar() {
+        return (
+            <View style={[{flexDirection: 'row'}]}>
+                <View style={[{flexDirection: 'row', flex: 2}]}>
+                    <Button
+                        icon={{
+                            name: "arrow-right",
+                            size: 15,
+                            color: "blue"
+                        }}
+                        type='clear'
 
-                <Button
-                    icon={{
-                        name: "arrow-right",
-                        size: 15,
-                        color: "blue"
-                    }}
-                    type= 'clear'
+                    />
+                    <Text style={[commonStyles.commonInputTextStyle]}>
+                        {"EOS/USDT"}
+                    </Text>
+                    <Text style={[styles.smallRedFont]}>
+                        -3.20%
+                    </Text>
+                </View>
+                <View style={[{flexDirection: 'row', flex: 1}]}>
 
-                />
-                <Text style={[ commonStyles.commonInputTextStyle ]}>
-                    {"EOS/USDT"}
-                </Text>
-                <Text style={[styles.smallRedFont]}>
-                    -3.20%
-                </Text>
+                    <Button
+                        icon={{
+                            name: "arrow-right",
+                            size: 15,
+                            color: "blue"
+                        }}
+                        type="clear"
+                    />
+                    <Button
+                        icon={{
+                            name: "arrow-right",
+                            size: 15,
+                            color: "blue"
+                        }}
 
-                <Button
-                    icon={{
-                        name: "arrow-right",
-                        size: 15,
-                        color: "blue"
-                    }}
-                    type="clear"
-                />
-                <Button
-                    icon={{
-                        name: "arrow-right",
-                        size: 15,
-                        color: "blue"
-                    }}
+                        type="clear"
 
-                    type="clear"
+                    />
 
-                />
+                    <Button
+                        icon={{
+                            name: "arrow-right",
+                            size: 15,
+                            color: "blue"
+                        }}
+                        type="clear"
+                    />
 
-                <Button
-                    icon={{
-                        name: "arrow-right",
-                        size: 15,
-                        color: "blue"
-                    }}
-                    type="clear"
-                />
+                </View>
             </View>
-
-
         )
 
     }
 
 
-    renderPriceBar (){
+    renderPriceBar() {
 
         return (
 
-            <View style={[commonStyles.customerRow]}>
+            <View style={[{flexDirection: 'row'}]}>
                 <View style={[commonStyles.customerRow]}>
                     <Text style={[styles.bigFontPrice]}>2.32</Text>
                     <Text style={[styles.smallGrayFont]}>=3.12 CNY</Text>
                 </View>
-                <View style={[commonStyles.customerRow]}>
-
-                </View>
             </View>
-
-
         )
+    }
 
+    renderEntrustView(dataDic) {
+        let buyList = new Array();
+        let sellList = new Array();
+
+        if (dataDic) {
+            buyList = dataDic["buyList"];
+            sellList = dataDic["sellList"];
+
+            let list = [];
+            let sellLista = [];
+
+            for (let i = 0; i < buyList.length; i++){
+                let dic = buyList[i];
+                list.push(this.renderInfoCell(i,dic['entrust_price'],dic['entrust_volume'],dic['no_completed_volume']))
+            }
+
+            for (let i = 0; i < sellList.length; i++){
+                let dic = sellList[i];
+                sellLista.push(this.renderInfoCell(i,dic['entrust_price'],dic['entrust_volume'],dic['no_completed_volume']))
+            }
+
+
+            for(let i = 0; i < 5 ;i++){
+                list.push(this.renderInfoCell(1,'22','33','444'));
+                sellLista.push(this.renderInfoCell(1,'22','33','444'));
+            }
+
+            return (
+                <View style={[{flexDirection: 'row', flex: 1}]}>
+                    <View style={[{ flex: 1}]}>
+                        <View style={[{flexDirection: 'row', padding: 2 ,borderColor: '#cccccc', borderWidth: 0.5}]}>
+                            <Text style={[{flexDirection: 'row', flex: 1}]}>买</Text>
+                            <Text style={[{flexDirection: 'row', flex: 2}]}>数量</Text>
+                            <Text style={[{flexDirection: 'row', flex: 1}]}>价格</Text>
+                        </View>
+                        {list}
+                    </View>
+
+                    <View style={[{ flex: 1}]}>
+                        <View style={[{flexDirection: 'row', padding: 2,borderColor: '#cccccc', borderWidth: 0.5}]}>
+                            <Text style={[{flexDirection: 'row', flex: 1}]}>卖</Text>
+                            <Text style={[{flexDirection: 'row', flex: 2}]}>数量</Text>
+                            <Text style={[{flexDirection: 'row', flex: 1}]}>价格</Text>
+                        </View>
+                        {sellLista}
+                    </View>
+                </View>
+            )
+
+        } else {
+            return (
+                <View/>
+
+            )
+        }
     }
 
 
-
-
-
+    renderInfoCell(index, entrust_price, entrust_volume, no_completed_volume) {
+        return (
+            <View style={[{flexDirection: 'row',padding: 2, borderColor: '#cccccc', borderWidth: 0.5 }]}>
+                <Text style={[{ flex: 1, paddingLeft:5}]}>{index}</Text>
+                <Text style={[{ flex: 2, paddingLeft:5}]}>{no_completed_volume}</Text>
+                <Text style={[{ flex: 1, paddingLeft:5}]}>{entrust_price}</Text>
+            </View>
+        )
+    }
 }
 
 
-
-
-
-
-
-
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
 
     smallRedFont: {
         color: '#e7234c',
@@ -174,11 +232,10 @@ const styles = StyleSheet.create( {
     },
 
 
-    smallIconButton:{
+    smallIconButton: {
 
         // backgroundColor: '#ddd',
     },
-
 
 
     data: {
@@ -189,7 +246,7 @@ const styles = StyleSheet.create( {
         borderWidth: 1 / PixelRatio.get(),
         color: '#777'
     }
-} );
+});
 
 export default TradePageView;
 
