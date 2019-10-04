cb8643109020d6e5ebde8f765754952b3d5d2b79
test <test@test.com>
2019-10-04 09:51:30 +0800

    Merge branch 'developer' of bitbucket.org:gtdollardev/gt-exchange-app into developer
    # Conflicts:
    #	src/pages/auth/AuthRegisterPage.js
    #	src/pages/auth/AuthRegisterPageView.js

--- src/pages/kline/KlinePageView.js
+++ src/pages/kline/KlinePageView.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import {InteractionManager, Platform, SafeAreaView, ScrollView, StyleSheet, View, WebView} from 'react-native';
-import {Button, ListItem, Text} from "react-native-elements";
+// import { WebView } from 'react-native-webview';
+// import {WebView} from 'react-native-webview';
+import { InteractionManager, SafeAreaView, StyleSheet, View, WebView } from 'react-native';
 import commonStyles from "../../styles/commonStyles";
-import {NavigationActions, StackActions} from "react-navigation";
-import Spinner from "react-native-loading-spinner-overlay";
-import I18n from "../../I18n";
 import * as env from '../../env';
-import Keys from "../../configs/Keys";
-import {BorderlessButton} from "react-native-gesture-handler";
-import {Ionicons} from "@expo/vector-icons";
 import io from 'socket.io-client';
 
-// import {WebView} from 'react-native-webview';
 
 class KlinePageView extends React.Component {
 
-    constructor(props) {
-        super(props);
+    constructor( props ) {
+        super( props );
         this.state = {
             isRequesting: false,
             socket: null,
@@ -24,10 +18,10 @@ class KlinePageView extends React.Component {
         }
     }
 
-    static navigationOptions = (props) => {
-        const {navigation} = props;
-        const {state, setParams} = navigation;
-        const {params} = state;
+    static navigationOptions = ( props ) => {
+        const { navigation } = props;
+        const { state, setParams } = navigation;
+        const { params } = state;
 
 
         return {
@@ -42,72 +36,81 @@ class KlinePageView extends React.Component {
     }
 
     loadData() {
-        this.setState({
+        this.setState( {
             isRequesting: true
-        });
+        } );
 
-        InteractionManager.runAfterInteractions(() => {
-            this.socket = io(env.webSocket);
-            if (!this.socket.connected) {
+        InteractionManager.runAfterInteractions( () => {
+            this.socket = io( env.webSocket );
+            if ( !this.socket.connected ) {
                 this.socket.connect();
             }
-            this.socket.on('connect', () => {
+            this.socket.on( 'connect', () => {
                 // console.log('connect:', this.socket.connected);
-                this.socket.emit('init', {
+                this.socket.emit( 'init', {
                     user_id: this.props.userInfo.user_id ? this.props.userInfo.user_id : 0,
                     coin_exchange_id: this.props.coin_exchange.coin_exchange_id,
                     range: this.state.range
-                });
-            });
-            this.socket.on('entrustList', (data) => {
-                this.setState({buyList: data.buyList});
-                this.setState({sellList: data.sellList});
-                if (!this.state.buyPrice) {
-                    this.setState({buyPrice: data.sellList.length > 0 ? data.sellList[data.sellList.length - 1].entrust_price : ''});
-                    this.setState({sellPrice: data.buyList.length > 0 ? data.buyList[0].entrust_price : ''});
+                } );
+            } );
+            this.socket.on( 'entrustList', ( data ) => {
+                this.setState( { buyList: data.buyList } );
+                this.setState( { sellList: data.sellList } );
+                if ( !this.state.buyPrice ) {
+                    this.setState( { buyPrice: data.sellList.length > 0 ? data.sellList[ data.sellList.length - 1 ].entrust_price : '' } );
+                    this.setState( { sellPrice: data.buyList.length > 0 ? data.buyList[ 0 ].entrust_price : '' } );
                 }
-            });
+            } );
 
-        })
+        } )
     }
 
     componentWillUnmount() {
-        this.setState = (state, callback) => {
+        this.setState = ( state, callback ) => {
         };
-        if (this.socket && this.socket.connected) {
-            this.socket.emit("disconnect");
+        if ( this.socket && this.socket.connected ) {
+            this.socket.emit( "disconnect" );
         }
     }
 
-    componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps( nextProps ) {
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
+    shouldComponentUpdate( nextProps, nextState ) {
         return true;
     }
 
 
     render() {
-        const {navigation} = this.props;
+        const { navigation } = this.props;
         return (
-            <View style={[commonStyles.wrapper,]}>
-                <SafeAreaView style={[commonStyles.wrapper]}>
-                    <View style={{height: 150, width: 150, overflow: 'hidden'}}>
-                        {/*<Text>{JSON.stringify(this.props.coin_exchange)}</Text>*/}
+            <View style={[ commonStyles.wrapper, ]}>
+                <SafeAreaView style={[ commonStyles.wrapper ]}>
+                    {/*<Text>{JSON.stringify(this.props.coin_exchange)}</Text>*/}
 
-                        {/*<WebView*/}
-                        {/*source={{uri: 'https://github.com/react-native-community/react-native-webview'}}*/}
-                        {/*originWhitelist={['*']}*/}
-                        {/*javaScriptEnabled={true}*/}
-                        {/*domStorageEnabled={true}*/}
-                        {/*startInLoadingState={true}*/}
-                        {/*allowUniversalAccessFromFileURLs={true}*/}
-                        {/*mixedContentMode={'compatibility'}*/}
-                        {/*/>*/}
-                        <WebView style={{backgroundColor: "black"}} originWhitelist={['*']}
-                                 source={{uri: 'https://github.com/facebook/react-native'}}
-                                 javaScriptEnabled={true}
-                                 domStorageEnabled={true}
+                    {/*<WebView*/}
+                    {/*source={{uri: 'https://github.com/react-native-community/react-native-webview'}}*/}
+                    {/*originWhitelist={['*']}*/}
+                    {/*javaScriptEnabled={true}*/}
+                    {/*domStorageEnabled={true}*/}
+                    {/*startInLoadingState={true}*/}
+                    {/*allowUniversalAccessFromFileURLs={true}*/}
+                    {/*mixedContentMode={'compatibility'}*/}
+                    {/*/>*/}
+
+                    {/*<WebView style={{ height: constStyles.window.width * 3 / 4, width: constStyles.window.width }}*/}
+                    {/*originWhitelist={[ '*' ]}*/}
+                    {/*source={{ uri: 'https://www.asiaedx.com/#/kline/6?lang=zh-cn' }}*/}
+                    {/*javaScriptEnabled={true}*/}
+                    {/*domStorageEnabled={true}*/}
+                    {/*/>*/}
+
+                    <View style={[ commonStyles.wrapper, commonStyles.commonBG, commonStyles.commonBorderTop ]}>
+                        <WebView
+                            source={{ uri: 'https://www.asiaedx.com/#/kline/6?lang=zh-cn' }}
+                            style={{ marginTop: 20 }}
+                            useWebKit={true}
+                            scrollEnabled={false}
                         />
                     </View>
                 </SafeAreaView>
@@ -117,7 +120,7 @@ class KlinePageView extends React.Component {
 }
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create( {} );
 
 export default KlinePageView;
 
